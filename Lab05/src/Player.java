7306ccc6def967222edeb7b4290e7f0d143d870c
Misaraka <68675841+Ungpy@users.noreply.github.com>
2023-02-17 12:42:18 +0700

    Add files via upload

---
+++ Lab05/src/Player.java
@@ -0,0 +1,23 @@
+
+public class Player {
+    private String name;
+    private String team;
+    
+    public void setName(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setTeam(String t){
+        team = t;
+    }
+    public String getTeam(){
+        return team;
+    }
+    public boolean isSameTeam(Player p){
+        boolean what;
+        what = (p.team.equals(this.team));
+        return what;
+    }
+}
