1daf614c37d54602fbaad572f6e63f6365bb617b
Misaraka <68675841+Ungpy@users.noreply.github.com>
2023-03-02 22:53:45 +0700

    Add files via upload

---
+++ Lab07/src/CheckingAccount.java
@@ -0,0 +1,49 @@
+
+public class CheckingAccount extends Account{
+    private double credit;
+    
+    public CheckingAccount(){
+        this(0, "", 0);
+    }
+    
+    public CheckingAccount(double balance, String name, double credit){
+        super(balance, name);
+        this.credit = credit;
+    }
+    
+    public void setCredit(double credit){
+        if(credit > 0){
+            this.credit = credit;
+        }
+        else{
+            System.out.println("Input number must be a positive integer.");
+        }
+    }
+    
+    public double getCredit(){
+        return credit;
+    }
+    
+    public void withdraw(double a){
+        if(this.balance >= a){
+            this.balance -= a;
+            System.out.println(a + " baht is withdrawn from " + this.name + " and your credit balance is " + this.credit);
+        }
+        else if((this.balance + this.credit) >= a){
+            this.credit -= a - balance;
+            this.balance = 0;
+            System.out.println(a + " baht is withdrawn from " + this.name + " and your credit balance is " + this.credit);
+        }
+        else{
+            System.out.println("Not enough money!");
+        }
+    }
+    
+    public void withdraw(String a){
+        this.withdraw(Double.parseDouble(a));
+    }
+    
+    public String toString(){
+        return ("The " + this.name+ " account has "+ this.balance+ " baht and " + this.credit+ " credits.");
+    }
+}
---
+++ Lab07/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+ public static void main(String[] args) {
+Account a1 = new CheckingAccount();
+ a1.setName("Nook");
+System.out.println(a1);
+((CheckingAccount)a1).setCredit(1000);
+System.out.println(a1); }
+}
