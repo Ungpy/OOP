a7eabae81b8f2ba49c3bd87067c12068ba68cdcc
Misaraka <68675841+Ungpy@users.noreply.github.com>
2023-03-06 23:21:45 +0700

    Add files via upload

--- Lab07/src/Main.java
+++ Lab07/src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
  public static void main(String[] args) {
- CheckingAccount a1 = new CheckingAccount(1000,"62070033",500);
- Customer c1 = new Customer();
- Customer c2 = new Customer("Harry","Potter");
- Customer c3 = new Customer("Harry","Potter",a1);
- System.out.println(c1.equals(c2));
- System.out.println(c3.equals(c2));
+ Plane p1 = new Plane(200,"IT Airline","FX-747");
+ Ship s1 = new Ship(200);
+
+ System.out.println("=== plane ===");
+ p1.startEngine();
+ p1.takeOff();
+ p1.fly();
+ p1.fly();
+ p1.honk();
+ p1.landing();
+ p1.stopEngine();
+
+ System.out.println("=== ship ===");
+ s1.startEngine();
+ s1.move(2);
+ s1.honk();
+ s1.stopEngine();
+
  }
 }
---
+++ Lab07/src/Ship.java
@@ -0,0 +1,53 @@
+
+public class Ship extends Vehicle implements Floatable{
+    public Ship(){
+        super();
+    }
+    public Ship(double fuel){
+        super(fuel);
+    }
+    
+    public void move(){
+        fl0at();
+    }
+    public void move(int distance){
+        for(int i=0; i< distance; i++){
+            if(this.getFuel() >= 50){
+                fl0at();
+            }
+            else{
+                System.out.println("Fuel is not enough.");
+                break;
+            }
+        }
+    }
+    
+    public void fl0at(){
+        if(this.getFuel() >= 50){
+            this.setFuel(this.getFuel() - 50);
+            System.out.println("Ship moves");
+        }
+        else{
+            System.out.println("Fuel is not enough.");
+        }
+    }
+    
+    public void startEngine(){
+        if(this.getFuel() >= 10){
+            this.setFuel(this.getFuel() - 10);
+            System.out.println("Engine starts");
+        }
+        else{
+            System.out.println("Fuel is not enough.");
+        }
+    }
+    
+    public void stopEngine(){
+        System.out.println("Engine stops");
+    }
+    
+    public void honk(){
+        System.out.println("Shhhhh");
+    }
+
+}
